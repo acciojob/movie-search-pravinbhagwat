acf0356c56f4765b233a456f9c6823cc2e081747
pravinbhagwat <pravinbhagwat150@gmail.com>
2023-06-27 15:34:30 +0530

    submitting code-Tue Jun 27 2023 15:34:30 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,51 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
+import axios from "axios";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+   let [name, setName] = useState("");
+   let [movies, setMovies] = useState([]);
 
-export default App
+   function handle(e) {
+      e.preventDefault();
+      axios
+         .get(`http://www.omdbapi.com/?s=${name}&apikey=696d88ef`)
+         .then((data) => (setMovies(data.data.Search), setError("")))
+         .catch((err) => console.log(err));
+   }
+
+   return (
+      <div>
+         <h2>Search Movies</h2>
+         <form
+            onSubmit={(e) => {
+               handle(e);
+            }}
+         >
+            <input
+               type="text"
+               onChange={(e) => {
+                  setName(e.target.value);
+               }}
+            ></input>
+            <button>Search</button>
+         </form>
+         <ul>
+            {movies ? (
+               movies.map((mov) => (
+                  <li>
+                     <h1>
+                        {mov.Title}({mov.Year})
+                     </h1>
+                     <img src={mov.Poster}></img>
+                  </li>
+               ))
+            ) : (
+               <p className="error">Invalid movie name. Please try again.</p>
+            )}
+         </ul>
+      </div>
+   );
+};
+
+export default App;
